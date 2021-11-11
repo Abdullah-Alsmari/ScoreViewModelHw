152329acba4c462a80f762c0b1394965d6164d70
Abdullah Alsmari <92260200+Abdullah-Alsmari@users.noreply.github.com>
2021-11-11 09:47:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/scoreteamhw/ui/main/MainViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.scoreteamhw.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+  
+    private var _scope = 0
+    val scope: Int
+        get() = _scope
+
+    fun plusone () {
+        _scope += 1
+    }
+
+    fun plusFour() {
+        _scope += 4
+    }
+
+    fun subTwo() {
+        if ( _scope >= 2) _scope -= 2
+    }
+
+}
